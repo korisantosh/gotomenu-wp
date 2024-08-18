cdf773460379ca293b53a94a349983413938333a
santoshkori-lilly <santosh.kori@network.lilly.com>
2024-08-18 13:37:41 +0530

    Refactored code

--- gotomenu.php
+++ gotomenu.php
@@ -14,15 +14,17 @@ if (!defined('ABSPATH')) {
 }
 // Include settings page
 require_once plugin_dir_path(__FILE__) . 'includes/gotomenu-settings.php';
+
 // Enqueue scripts and styles
-function gotomenu_enqueue_scripts()
-{
-    if (get_option('gotomenu_enable_frontend') == '1') {
+function gotomenu_enqueue_scripts() {
+    if (get_option('gotomenu_enable_frontend') === '1') {
         // Get file modification time for cache busting
         $css_version = filemtime(plugin_dir_path(__FILE__) . 'assets/css/gotomenu.css');
         $js_version = filemtime(plugin_dir_path(__FILE__) . 'assets/js/gotomenu.js');
+
         wp_enqueue_style('gotomenu-style', plugin_dir_url(__FILE__) . 'assets/css/gotomenu.css', array(), $css_version);
         wp_enqueue_script('gotomenu-script', plugin_dir_url(__FILE__) . 'assets/js/gotomenu.js', array('jquery'), $js_version, true);
+
         // Localize script to pass PHP data to JavaScript securely
         wp_localize_script('gotomenu-script', 'gotomenuData', array(
             'menus' => gotomenu_get_menus()
@@ -31,17 +33,13 @@ function gotomenu_enqueue_scripts()
 }
 add_action('wp_enqueue_scripts', 'gotomenu_enqueue_scripts');
 
-
-function gotomenu_admin_enqueue_scripts()
-{
+// Enqueue scripts and styles for admin
+function gotomenu_admin_enqueue_scripts() {
     // Get file modification time for cache busting
     $css_version = filemtime(plugin_dir_path(__FILE__) . 'assets/css/gotomenu.css');
     $js_version = filemtime(plugin_dir_path(__FILE__) . 'assets/js/gotomenu-admin.js');
 
-    // Enqueue admin styles for all admin pages
     wp_enqueue_style('gotomenu-admin-style', plugin_dir_url(__FILE__) . 'assets/css/gotomenu.css', array(), $css_version);
-
-    // Enqueue admin scripts for all admin pages
     wp_enqueue_script('gotomenu-admin-script', plugin_dir_url(__FILE__) . 'assets/js/gotomenu-admin.js', array('jquery'), $js_version, true);
 
     // Localize script to pass PHP data to JavaScript securely
@@ -50,9 +48,9 @@ function gotomenu_admin_enqueue_scripts()
     ));
 }
 add_action('admin_enqueue_scripts', 'gotomenu_admin_enqueue_scripts');
+
 // Function to get available menus
-function gotomenu_get_menus()
-{
+function gotomenu_get_menus() {
     $menu_items = array();
     $wp_menus = wp_get_nav_menus();
     foreach ($wp_menus as $menu) {
@@ -63,22 +61,24 @@ function gotomenu_get_menus()
                     'title' => esc_html($item->title),
                     'url'   => esc_url($item->url)
                 );
-                error_log("Single menu_item");
-                error_log(print_r($menu_items, true));
             }
         }
     }
+
     usort($menu_items, function ($a, $b) {
         return strcmp($a['title'], $b['title']);
     });
+
     // Remove specific item based on URL
     $menu_items = array_filter($menu_items, function ($item) {
         return $item['url'] !== esc_url(get_bloginfo('url'));
     });
+
     $homeItem = array(
         'title' => __('Home', 'gotomenu'),
         'url'  => esc_url(get_bloginfo('url'))
     );
+
     array_unshift($menu_items, $homeItem);
     return $menu_items;
 }
@@ -105,44 +105,39 @@ function get_menu_title($menu_title)
 }
 
 // Function to get available menus
-function gotomenu_get_admin_menus()
-{
+function gotomenu_get_admin_menus() {
     global $menu;
     $admin_menu_items = array();
-     error_log("Admin menus");
-    // error_log(print_r($menu, true));
 
     foreach ($menu as $item) {
-        // error_log("Single admin menu 4th items===" . $item[4]);
         if(isset($item[4]) && $item[4] !== 'wp-menu-separator') {
             $menu_title = get_menu_title($item[0]);
             $menu_icon = (isset($item[6])) ? $item[6] : 'dashicons-admin-generic';
+
             $admin_menu_items[] = array(
                 'title' => $menu_title,  // The menu title
                 'url'   => esc_url(admin_url($item[2])), // The URL to the admin page
                 'icon'   => $menu_icon // The menu icon
             );
-            // error_log("Inside adding Single admin menu item");
-            // error_log(print_r($admin_menu_items, true));
         }
     }
+
     usort($admin_menu_items, function ($a, $b) {
         return strcmp($a['title'], $b['title']);
     });
+
     $homeItem = array(
         'title' => __('Visit Site', 'gotomenu'),
         'url'  => esc_url(get_bloginfo('url')),
         'icon'   => 'dashicons-admin-site'
     );
+
     array_unshift($admin_menu_items, $homeItem);
-    // error_log("Admin menu items");
-    // error_log(print_r($admin_menu_items, true));
     return $admin_menu_items;
 }
 
 // Add settings link on plugin page
-function gotomenu_add_plugin_link($links)
-{
+function gotomenu_add_plugin_link($links) {
     $settings_link = '<a href="' . admin_url('options-general.php?page=gotomenu') . '">' . __('Settings', 'gotomenu') . '</a>';
     array_unshift($links, $settings_link);
     return $links;
--- includes/gotomenu-settings.php
+++ includes/gotomenu-settings.php
@@ -1,10 +1,17 @@
 <?php
 
 // Register settings
-function gotomenu_register_settings()
-{
+function gotomenu_register_settings() {
     add_option('gotomenu_enable_frontend', '0');
+    add_option('gotomenu_enable_backend', '0');
+
     register_setting('gotomenu_options_group', 'gotomenu_enable_frontend', array(
+        'type' => 'string',
+        'sanitize_callback' => 'sanitize_text_field',
+        'default' => '0'
+    ));
+
+    register_setting('gotomenu_options_group', 'gotomenu_enable_backend', array(
         'type' => 'string',
         'sanitize_callback' => 'sanitize_text_field',
         'default' => '0'
@@ -13,18 +20,22 @@ function gotomenu_register_settings()
 add_action('admin_init', 'gotomenu_register_settings');
 
 // Create settings page
-function gotomenu_register_options_page()
-{
-    add_options_page('GoToMenu Settings', 'GoToMenu', 'manage_options', 'gotomenu', 'gotomenu_options_page');
+function gotomenu_register_options_page() {
+    add_options_page(
+        __('GoToMenu Settings', 'gotomenu'),
+        __('GoToMenu', 'gotomenu'),
+        'manage_options',
+        'gotomenu',
+        'gotomenu_options_page'
+    );
 }
 add_action('admin_menu', 'gotomenu_register_options_page');
 
 // Display settings page
-function gotomenu_options_page()
-{
+function gotomenu_options_page() {
 ?>
     <div class="wrap">
-        <h2><?php esc_html_e('GoToMenu Settings', 'gotomenu'); ?></h2>
+    <h2><?php esc_html_e('GoToMenu Settings', 'gotomenu'); ?></h2>
         <form method="post" action="options.php">
             <?php settings_fields('gotomenu_options_group'); ?>
             <?php do_settings_sections('gotomenu'); ?>
@@ -39,16 +50,16 @@ function gotomenu_options_page()
                 <tr valign="top">
                     <th scope="row"><?php esc_html_e('Enable for Backend Admin Area', 'gotomenu'); ?></th>
                     <td>
-                        <input type="checkbox" id="gotomenu_enable_frontend" name="gotomenu_enable_frontend" value="1" <?php checked('1', get_option('gotomenu_enable_backend')); ?> />
-                        <label for="gotomenu_enable_frontend"><?php esc_html_e('Yes', 'gotomenu'); ?></label>
+                        <input type="checkbox" id="gotomenu_enable_backend" name="gotomenu_enable_backend" value="1" <?php checked('1', get_option('gotomenu_enable_backend')); ?> />
+                        <label for="gotomenu_enable_backend"><?php esc_html_e('Yes', 'gotomenu'); ?></label>
                     </td>
                 </tr>
             </table>
             <div class="notice inline notice-info notice-alt">
-                <p><span class="dashicons dashicons-info"></span> Press <strong>F2</strong> to use GoToMenu and instantly redirect to your selected page.</p>
+                <p><span class="dashicons dashicons-info"></span> <?php esc_html_e('Press F2 to use GoToMenu and instantly redirect to your selected page.', 'gotomenu'); ?></p>
             </div>
             <?php submit_button(); ?>
         </form>
     </div>
-<?php
+    <?php
 }
